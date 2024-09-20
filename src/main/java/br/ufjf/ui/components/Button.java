26ccf8b01591268afb7cca08bb26ff7f9a2657fd
João Pedro Martins Cruz <103667165+martins-joaopedro@users.noreply.github.com>
2024-09-20 16:57:52 -0300

    feat: adiciona icone no botao de voltar

--- src/main/java/br/ufjf/ui/components/Button.java
+++ src/main/java/br/ufjf/ui/components/Button.java
@@ -3,17 +3,22 @@ package br.ufjf.ui.components;
 import javax.swing.*;
 
 import br.ufjf.ui.Manager;
+import br.ufjf.ui.UIConstants;
+
+import java.awt.*;
 
 public class Button extends JButton {
     
     
     public Button(String text) {
 
-        ImageIcon icon = new ImageIcon("content/icon.png");
-
-        setIcon(icon);
-        
         addActionListener(e -> Manager.navigateBack());
+        setPreferredSize(new Dimension(30, 30));
+        setMargin(new Insets(0, 0, 0, 0));
+        add(new Image("content/icon.png", UIConstants.ICON_SIZE, UIConstants.ICON_SIZE));
+        setHorizontalAlignment(SwingConstants.CENTER);
+        setVerticalAlignment(SwingConstants.CENTER);
+        setBackground(Color.WHITE);
     }
 
     public Button(String text, String screenToNav) {
