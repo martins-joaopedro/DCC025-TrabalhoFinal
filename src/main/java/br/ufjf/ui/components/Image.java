a55d622fc96f83083467b450eaf21ee0779baaf4
João Pedro Martins Cruz <103667165+martins-joaopedro@users.noreply.github.com>
2024-09-20 16:57:18 -0300

    feat: implementa componente imagem

---
+++ src/main/java/br/ufjf/ui/components/Image.java
@@ -0,0 +1,50 @@
+package br.ufjf.ui.components;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Image extends JPanel {
+
+    int width;
+    int height;
+    int x;
+    int y;
+    BufferedImage img;
+
+    public Image(String path, int width, int height, int x, int y) {
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+
+        try {
+            this.img = ImageIO.read(new File(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Image(String path, int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.x = 0;
+        this.y = 0;
+
+        try {
+            this.img = ImageIO.read(new File(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(img, this.x, this.y, this.width, this.height, this);
+
+    }
+}
