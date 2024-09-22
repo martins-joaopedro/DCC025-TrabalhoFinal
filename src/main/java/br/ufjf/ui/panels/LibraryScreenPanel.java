e9e8f46411fa53d43d961d8c0d95b4f91a57114b
João Pedro Martins Cruz <103667165+martins-joaopedro@users.noreply.github.com>
2024-09-21 21:10:17 -0300

    refactor: usa o componentList na tela de biblioteca

--- src/main/java/br/ufjf/ui/panels/LibraryScreenPanel.java
+++ src/main/java/br/ufjf/ui/panels/LibraryScreenPanel.java
@@ -1,17 +1,15 @@
 package br.ufjf.ui.panels;
 
-import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.Box;
-import javax.swing.JLabel;
+import javax.swing.BoxLayout;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 
 import br.ufjf.models.Book;
 import br.ufjf.services.LibraryService;
 import br.ufjf.ui.components.BookCard;
-import br.ufjf.ui.UIConstants;
+import br.ufjf.ui.components.ComponentList;
 
 public class LibraryScreenPanel extends JPanel {
 
@@ -19,28 +17,20 @@ public class LibraryScreenPanel extends JPanel {
 
     public LibraryScreenPanel() {
 
-        add(new JLabel("Acervo"));
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         
         loadBooks();
+
     }
 
     public void loadBooks() {
         List<Book> books = service.findAll();
-        
-        JPanel container = new JPanel();
-        container.setLayout(new FlowLayout());
-        //container.setPreferredSize(new Dimension(100, 100));
-        container.add(Box.createHorizontalStrut(20));
+        List<BookCard> bookCards = new ArrayList<>();
 
-        for (Book book : books)
-            container.add(new BookCard(book));
+        for(Book book : books)
+            bookCards.add(new BookCard(book));
 
-        JScrollPane scroll = new JScrollPane(container);
-        scroll.getHorizontalScrollBar().setUnitIncrement(16);
-        scroll.setPreferredSize(new Dimension(UIConstants.SCREEN_WIDTH-50, UIConstants.SCREEN_HEIGHT));
-        scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS); // Define o scroll horizontal
-        scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER); // Remove o scroll vertical
-
-        add(scroll);
+        ComponentList<BookCard> list = new ComponentList<>(bookCards, false);
+        add(list);
     }
 }
